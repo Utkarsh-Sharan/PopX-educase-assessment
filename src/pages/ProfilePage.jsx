880849374c3c7321d33ecff047d5918ad8e566ec
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-07-06 17:45:24 +0530

    add rubik text and some polishing for all pages

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,29 +3,32 @@ import Cam from "../assets/cam.svg";
 
 const ProfilePage = () => {
   return (
-    <>
+    <section className="font-rubik">
         <nav className='absolute top-0 left-0 w-full bg-white px-2 py-4 shadow-md'>
             Account Settings
         </nav>
 
-        <article className="mt-16 flex flex-col gap-2">
-            <article className="flex items-start justify-start gap-5">
-                <div className="relative">
-                    <img src={Profile} alt="profile" />
-                    <img className="absolute bottom-0 right-0" src={Cam} alt="change-profile" />
-                </div>
-                <div>
-                    <h5 className="font-semibold">Marry Doe</h5>
-                    <p>Marry@Gmail.Com</p>
-                </div>
+        <section className="flex flex-col gap-10">
+            <article className="mt-16 flex flex-col gap-2">
+                <article className="flex items-start justify-start gap-5">
+                    <div className="relative">
+                        <img src={Profile} alt="profile" />
+                        <img className="absolute bottom-0 right-0" src={Cam} alt="change-profile" />
+                    </div>
+                    <div>
+                        <h5 className="font-semibold">Marry Doe</h5>
+                        <p>Marry@Gmail.Com</p>
+                    </div>
+                </article>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Labore fuga tempore eum veritatis consequatur ea quaerat
+                    esse eos voluptatem ex!
+                </p>
             </article>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Labore fuga tempore eum veritatis consequatur ea quaerat
-                esse eos voluptatem ex!
-            </p>
-        </article>
-    </>
+            <div className="border-b border-dotted border-gray-400 mt-10"></div>
+        </section>
+    </section>
   )
 }
 
